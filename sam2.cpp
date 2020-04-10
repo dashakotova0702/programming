74ea94a90e29d432561c1bf2e6935621346d029e
dashakotova0702 <dashakotova0702@gmail.com>
2020-04-10 16:20:01 +0300

    Удивительная контроша 2.0

--- sam2.cpp
+++ sam2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   if (argc != 3) {
     cout << "Неверное количество параметров" << endl;
-    return 1;  //в интернете написали, что это закрытие программы с ошибкой, наверное, это нужно
+    return 1;
   }
   setlocale(LC_ALL, "rus");
   ifstream file1;
@@ -23,9 +23,8 @@ int main(int argc, char const *argv[]) {
   }
   string str1, str2;
   int i, length;
-  while(!file1.eof()){
+  while(!file1.eof()){      //короч я не знаю как можно переделать этот цикл не посимвольно
     getline (file1, str1);
-    cout << str1 << endl; //чисто проверочка
     length = str1.length();
     for (i = 0; i < length; i++) {
       if (str1[i] == ' '){
@@ -36,22 +35,12 @@ int main(int argc, char const *argv[]) {
         str2 += str1[i];
     }
   }
-    cout << str2 << endl; //чисто проверочка
-    length = str2.length();
-    if (str2[0] == ' ' || str2[length-1] == ' ') {
-      if (str2[length-1] == ' ') {
-        for (i = 1; i < (length-1); i++) {
-          file2 << str2[i];
-        }
-      }
-      else {
-        for (i = 1; i < length; i++){
-          file2 << str2[i];
-        }
-      }
-    }
-    else
-      file2 << str2;
+  length = str2.length();
+  if (str2[length-1] == ' ') //тут сделала по красоте
+    str2.erase(length-1, 1);
+  if (str2[0] == ' ')
+    str2.erase(0, 1);
+  file2 << str2;
   file1.close();
   file2.close();
   return 0;
