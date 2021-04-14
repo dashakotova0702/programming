8e857097b35016f068b502caf67acbe20c409694
dashakotova0702 <dashakotova0702@gmail.com>
2021-04-14 15:17:56 +0300

    laba

--- Zhvnrchk/laba1.cpp
+++ Zhvnrchk/laba1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Grammar {
 public:
-string programm = "name; float: for;";
+string programm = "name;float:for;";
 string reserved[15] = {"int", "float", "bool", "ass", "if", "then", "else", "for", "to", "do", "while", "read", "write", "true", "false"};
 string separators[20] = {" ", "{", "}", ";", "(", ")", ":", "/", "\0", "[", "]", "<", ">", "=", "+", "-", "*", "or", "and", "not"};
 list <string> names;
@@ -16,47 +16,38 @@ list <int> id_element;
 void scanner (Grammar* g) {
         string str;
         smatch m;
-        int position = 0;
-        int count_names = 0;
+        int count_names = -1;
         //for (int i = 0; i < 20; i++) {
         const regex sep(";|:");
-        if (regex_search(g->programm, m, sep)) {
-                cout << "!" << endl;
+        while (regex_search(g->programm, m, sep)) {
                 int start = m.position();
-                int end = m.position() + m.size();
                 str.clear();
-                if (start == 0) {
-                        g->id_table.push_back(1);
-                        g->id_element.push_back(8);
+                for (int j = 0; j < start; j++) {
+                        str += g->programm[j];
                 }
-                else {
-                        for (int j = position; j < start; j++) {
-                                str += g->programm[j];
+                int count = 0;
+                for (int j = 0; j < 15; j++) {
+                        if (str == g->reserved[j]) {
+                                g->id_table.push_back(0);
+                                g->id_element.push_back(j);
+                                count++;
                         }
-                        position = end++;
-                        int count = 0;
-                        for (int j = 0; j < 15; j++) {
-                                if (str == g->reserved[j]) {
-                                        g->id_table.push_back(0);
-                                        g->id_element.push_back(j);
-                                        count++;
-                                }
-                        }
-                        if (count == 0) {
-                                count_names++;
-                                g->names.push_back(str);
-                                g->id_table.push_back(2);
-                                g->id_element.push_back(count_names);
-                        }
-                        g->id_table.push_back(1);
-                        g->id_element.push_back(8);
                 }
-
+                if (count == 0) {
+                        count_names++;
+                        g->names.push_back(str);
+                        g->id_table.push_back(2);
+                        g->id_element.push_back(count_names);
+                }
+                g->id_table.push_back(1);
+                g->id_element.push_back(8);
+                g->programm = m.suffix();
         }
         //}
+        cout << "Number of table:   ";
         for (int n : g->id_table)
                 cout << n << " ";
-        cout << endl;
+        cout << endl << "Number of element: ";
         for (int n : g->id_element)
                 cout << n << " ";
         cout << endl;
